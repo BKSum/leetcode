e15c417571d2c1c5341c5ed2394f7712b7af66ee
Brandon <brandon.sum@gmail.com>
2018-02-18 11:49:40 -0500

    move zeroes

--- src/main/java/bksum/leetcode/fb/array/MoveZeroes.java
+++ src/main/java/bksum/leetcode/fb/array/MoveZeroes.java
@@ -11,19 +11,37 @@ import java.util.*;
  */
 public class MoveZeroes {
 
-    public void moveZeroes(int[] nums) {
-    	//0 0 1
-    	//0 1 0
-    	//0 
+    public void moveZeroesOn3(int[] nums) {
         for(int i=0; i<nums.length; i++) {
-        	if(nums[i] == 0) {
-        		for(int j=i;j<nums.length-1; j++) {
-        			nums[j] = nums[j+1];
-        		}
-        		nums[nums.length-1] = 0;
-        		break;
-        	}
+    		for(int j=i;j<nums.length-1; j++) {
+    			if(nums[i] == 0) {
+    				for(int k=i;k<nums.length-1;k++) {
+    					nums[k] = nums[k+1];
+    				}
+            		nums[nums.length-1] = 0;
+    			}else {
+    				break;
+    			}
+    		}
         }
+    }
+    
+    public void moveZeroes(int[] nums) {
+    	int arrayCounter = 0;
+
+    	if(nums == null || nums.length == 0) { 
+    		return;
+    	}
+    	
+    	for(int pointer : nums) {
+    		if(pointer != 0) {
+    			nums[arrayCounter++] = pointer;
+    		}
+    	}
+    	
+    	for(int i = arrayCounter; i<nums.length; i++) {
+    		nums[i] = 0;
+    	}
     }
     
 	/**
@@ -31,7 +49,7 @@ public class MoveZeroes {
 	 */
 	public static void main(String[] args) {
         //Scanner in = new Scanner(System.in);
-		int[] numArray = new int[] {0, 0, 1};
+		int[] numArray = new int[] {0, 1, 0, 3, 12};
 
 		MoveZeroes mz = new MoveZeroes();
 		mz.moveZeroes(numArray);
