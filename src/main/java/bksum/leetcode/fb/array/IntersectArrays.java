1d15dbffbf4843f70739020c0c7ad6307700490e
Brandon <brandon.sum@gmail.com>
2018-02-19 13:14:57 -0500

    updated

--- src/main/java/bksum/leetcode/fb/array/IntersectArrays.java
+++ src/main/java/bksum/leetcode/fb/array/IntersectArrays.java
@@ -3,11 +3,38 @@
  */
 package bksum.leetcode.fb.array;
 
+import java.util.*;
+
 /**
  * @author Brandon
  *
  */
 public class IntersectArrays {
+	
+	private int[] findCommon(int[] bigger, int[] smaller) {
+		int counter = 0;
+    	List<Integer> intersectList = new ArrayList<Integer>();
+
+		for(int i=0; i<bigger.length; i++) {
+			if(bigger[i]==smaller[counter]) {
+				intersectList.add(bigger[i]);
+				counter++;
+			}else if(bigger[i]<smaller[counter]) {
+				continue;
+			}else{
+				counter++;
+				i -= 1;
+			} 
+			if(counter > smaller.length-1){
+				break;
+			}
+		}
+        int[] res = new int[intersectList.size()];
+        for(int i = 0; i<res.length; i++){
+            res[i] = (Integer)intersectList.get(i);
+        }
+		return res;
+	}
 	
 	/**
 	 * 
@@ -16,6 +43,11 @@ public class IntersectArrays {
 	 * @return
 	 */
     public int[] intersect(int[] nums1, int[] nums2) {
-		return nums2;
+    	if(nums1.length == 0 || nums2.length == 0) {
+    		return (new int[0]);
+    	}
+    	Arrays.sort(nums1);
+    	Arrays.sort(nums2);
+		return findCommon(nums1, nums2);
     }
 }
--- src/test/java/bksum/leetcode/fb/array/IntersectArraysTest.java
+++ src/test/java/bksum/leetcode/fb/array/IntersectArraysTest.java
@@ -33,4 +33,27 @@ public class IntersectArraysTest {
 		assertArrayEquals(expectedResults, ia.intersect(nums1, nums2));
 	}
 
+	/**
+	 * Test method for {@link bksum.leetcode.fb.array.IntersectArrays#intersect(int[], int[])}.
+	 */
+	@Test
+	public void testIntersectRainy1() {
+		IntersectArrays ia = new IntersectArrays();
+		int[] nums1 = new int[] {1,2};
+		int[] nums2 = new int[] {1,1};
+		int[] expectedResults = new int[] {1};
+		assertArrayEquals(expectedResults, ia.intersect(nums1, nums2));
+	}
+	
+	/**
+	 * Test method for {@link bksum.leetcode.fb.array.IntersectArrays#intersect(int[], int[])}.
+	 */
+	@Test
+	public void testIntersectRainy2() {
+		IntersectArrays ia = new IntersectArrays();
+		int[] nums1 = new int[] {};
+		int[] nums2 = new int[] {};
+		int[] expectedResults = new int[] {};
+		assertArrayEquals(expectedResults, ia.intersect(nums1, nums2));
+	}
 }
