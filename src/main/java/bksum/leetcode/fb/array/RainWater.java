cc9936ff76ddd3801c860468ea3599333f2c9300
Brandon <brandon.sum@gmail.com>
2018-02-19 09:48:10 -0500

    updated.
    Change-Id: Ifdf4eab6bdf8cd73f90df4a88ab26a2a659a1fd9

--- src/main/java/bksum/leetcode/fb/array/RainWater.java
+++ src/main/java/bksum/leetcode/fb/array/RainWater.java
@@ -8,7 +8,8 @@ package bksum.leetcode.fb.array;
  *
  */
 public class RainWater {
-    public int trap(int[] height) {
+	
+    public int trapN2(int[] height) {
     	int waterAmount = 0;
     	for(int i = 1; i< height.length-1; i++) {
         	int maxLeft = 0;
@@ -27,16 +28,38 @@ public class RainWater {
 		return waterAmount;
     }
     
+    public int trap(int[] height) {
+    	int heightSize = height.length;
+    	int[] leftMost = new int[heightSize];
+    	int[] rightMost = new int[heightSize];
+    	int answer = 0;
+    	
+    	for(int i=0; i<heightSize; i++) {
+    		leftMost[i] = (i==0) ? height[i] : Math.max(leftMost[i-1], height[i]);
+    	}
+    	
+    	for(int i=heightSize-1; i>=0; i--) {
+    		rightMost[i] = (heightSize-1==i) ? height[i] : Math.max(rightMost[i+1], height[i]);
+    	}
+    	
+    	for(int i=0; i<heightSize; i++) {
+    		answer += Math.min(leftMost[i], rightMost[i]) - height[i];
+    	}
+    	
+		return answer;
+    }
+    
+    
+    
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
         //Scanner in = new Scanner(System.in);
-		int[] elevationMapa = new int[] {0,1,0,2,1,0,1,3,2,1,2,1};
-		int[] elevationMap = new int[] {0,1,0,2,1,0,1,3};
+		int[] elevationMap = new int[] {0,1,0,2,1,0,1,3,2,1,2,1};
 
 		RainWater ab = new RainWater();
-		System.out.println(ab.trap(elevationMapa));
+		System.out.println(ab.trap(elevationMap));
 	}
 
 }
