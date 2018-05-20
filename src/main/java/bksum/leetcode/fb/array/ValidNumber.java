bf960fd75e9c7556222d6fa3bec8857b82844e0a
Brandon <brandon.sum@gmail.com>
2018-05-20 09:34:33 -0400

    updated

--- src/main/java/bksum/leetcode/fb/array/ValidNumber.java
+++ src/main/java/bksum/leetcode/fb/array/ValidNumber.java
@@ -13,31 +13,39 @@ public class ValidNumber {
     public boolean isNumber(String s) {
         s = s.trim();
         if(s.length() == 0) return false;
-
         boolean hasDecimal = false;
         boolean hasE = false;
+        boolean atLeastOneInteger = false;
+        boolean numberBeforeE = false;
+        boolean numberAfterE = false;
+
         for(int i=0; i<s.length(); i++){
             char c = s.charAt(i);
             if(c == '.'){
                 if(hasDecimal) return false;
                 if(s.length() == 1) return false;
+                if(hasE) return false;
                 hasDecimal = true;
             }else if (c=='e'){
                 if(hasE) return false;
                 if(i == 0) return false;
+                if(i == s.length()-1) return false;
+                if(!numberBeforeE) return false;
+                if(!Character.isDigit(s.charAt(i+1)) && s.charAt(i+1) != '+' && s.charAt(i+1) != '-') return false;
                 hasE = true;
+            }else if(c=='-' || c=='+'){
+                if(i==0) continue;
+                if(s.charAt(i-1) == 'e') continue;
+                return false;
             }else if(!Character.isDigit(c)){
                 return false;
+            }else{
+                atLeastOneInteger = true;
+                numberBeforeE = !hasE;
+                numberAfterE = hasE;
             }
         }
-        return true;
+        if(hasE && !numberAfterE) return false;
+        return atLeastOneInteger;
     }
-
-//    @SuppressWarnings("all")
-//    public boolean isNumberRegex(String s) {
-//        s = s.trim();
-//        if(s.isEmpty())
-//            return false;
-//        return s.trim().matches("[+-]?[0-9]*((?<=[0-9])\\.||\\.(?=[0-9]))?[0-9]*((?<=[0-9.])(e[+-]?[0-9]+))?");
-//    }
 }
--- src/test/java/bksum/leetcode/fb/array/ValidNumberTest.java
+++ src/test/java/bksum/leetcode/fb/array/ValidNumberTest.java
@@ -38,4 +38,49 @@ public class ValidNumberTest {
     public void isNumberTest4() {
         assertTrue(vn.isNumber("2e10"));
     }
+
+    @Test
+    public void isNumberTest5() {
+        assertFalse(vn.isNumber("0e"));
+    }
+
+    @Test
+    public void isNumberTest6() {
+        assertFalse(vn.isNumber(".e1"));
+    }
+
+    @Test
+    public void isNumberTest7() {
+        assertFalse(vn.isNumber("1e."));
+    }
+
+    @Test
+    public void isNumberTest8() {
+        assertTrue(vn.isNumber("-1."));
+    }
+
+    @Test
+    public void isNumberTest9() {
+        assertFalse(vn.isNumber("-."));
+    }
+
+    @Test
+    public void isNumberTest10() {
+        assertTrue(vn.isNumber("46.e3"));
+    }
+
+    @Test
+    public void isNumberTest11() {
+        assertFalse(vn.isNumber("6e6.5"));
+    }
+
+    @Test
+    public void isNumberTest12() {
+        assertTrue(vn.isNumber(" 005047e+6"));
+    }
+
+    @Test
+    public void isNumberTest13() {
+        assertFalse(vn.isNumber("4e+"));
+    }
 }
