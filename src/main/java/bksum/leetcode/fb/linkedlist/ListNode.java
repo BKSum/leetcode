6b6632df37d13d07a7be5918b086f15b8ce3c779
Brandon <brandon.sum@gmail.com>
2018-09-06 08:07:26 -0400

    updated

--- src/main/java/bksum/leetcode/fb/linkedlist/ListNode.java
+++ src/main/java/bksum/leetcode/fb/linkedlist/ListNode.java
@@ -7,4 +7,15 @@ public class ListNode {
 	ListNode(int x) {
 		val = x;
 	}
+
+	public String toString(){
+		StringBuilder sb = new StringBuilder();
+		ListNode pointer = this;
+		while(pointer!=null){
+			sb.append(pointer.val);
+			if(pointer.next != null) sb.append("->");
+			pointer = pointer.next;
+		}
+		return sb.toString();
+	}
 }
