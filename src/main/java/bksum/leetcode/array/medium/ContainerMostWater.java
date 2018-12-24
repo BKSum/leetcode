bd36f7734c2ae472bede6ca72b98cfa7c4d00f9f
Brandon <brandon.sum@gmail.com>
2018-12-24 08:48:29 -0500

    Adding

--- src/main/java/bksum/leetcode/array/medium/ContainerMostWater.java
+++ src/main/java/bksum/leetcode/array/medium/ContainerMostWater.java
@@ -4,6 +4,12 @@ package bksum.leetcode.array.medium;
  * Problem #11
  */
 public class ContainerMostWater {
+
+    /**
+     *
+     * @param height
+     * @return
+     */
     public int maxArea(int[] height) {
         return 0;
     }
