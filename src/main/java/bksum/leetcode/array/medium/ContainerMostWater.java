1412160e2343595c8ed15352ef7fef30a6310b9a
Brandon <brandon.sum@gmail.com>
2018-12-24 08:32:04 -0500

    Adding

---
+++ src/main/java/bksum/leetcode/array/medium/ContainerMostWater.java
@@ -0,0 +1,10 @@
+package bksum.leetcode.array.medium;
+
+/**
+ * Problem #11
+ */
+public class ContainerMostWater {
+    public int maxArea(int[] height) {
+        return 0;
+    }
+}
---
+++ src/test/java/bksum/leetcode/array/medium/ContainerMostWaterTest.java
@@ -0,0 +1,22 @@
+package bksum.leetcode.array.medium;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ContainerMostWaterTest {
+
+    private ContainerMostWater cmw;
+
+    @Before
+    public void setUp(){
+        cmw = new ContainerMostWater();
+    }
+
+    @Test
+    public void maxArea() {
+        int[] height = new int[] {1,8,6,2,5,4,8,3,7};
+        assertEquals(49, cmw.maxArea(height));
+    }
+}
